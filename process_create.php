f33e8047e98438ebe11e84a7d085f65c0ff852f3
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-03-09 16:26:41 +0900

    입력 공격의 차단
    mysqli_real_escape_string($conn, $something)

--- process_create.php
+++ process_create.php
@@ -4,6 +4,12 @@ $conn = mysqli_connect(
   'root',
   '111111',
   'opentutorials');
+
+$filterd = array(
+    'title' => mysqli_real_escape_string($conn, $_POST['title']);
+    'description' => mysqli_real_escape_string($conn, $_POST['description']);
+);
+
 $sql = "
   INSERT INTO topic
     (title, description, created)
